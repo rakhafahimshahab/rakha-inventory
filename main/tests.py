e5e7ac6ea216ea22f0d4f28c91ef921e91919988
Rakha Fahim Shahab <rakha.fahim@ui.ac.id>
2023-11-30 19:11:02 +0700

    tests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase,Client
 
-# Create your tests here.
+class MainTest(TestCase):
+    def test_main_url_is_exist(self):
+        response = Client().get('/main/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_using_main_template(self):
+        response = Client().get('/main/')
+        self.assertTemplateUsed(response, 'main.html')
+
+    def test_show_main(self):
+        response = Client().get('/main/')
+        self.assertTemplateUsed(response, 'main.html')
+
+        self.assertEqual(response.context['application_name'], 'Pirates Database')
+        self.assertEqual(response.context['name'], 'Rakha Fahim Shahab')
+        self.assertEqual(response.context['class'], 'PBP KI')
